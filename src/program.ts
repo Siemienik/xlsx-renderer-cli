ec52c4b46fd1496e3a8d91f0a751ac81bdfd9ae1
Paweł @Siemienik <siemienik.pawel@gmail.com>
2020-08-16 12:11:10 +0200

    Implement command line interface for xlsx-renderer ISSUE: https://github.com/Siemienik/xlsx-renderer/issues/25
    todo:
    * readme
    * tests coverage
    * further proposal: xml support

--- src/program.ts
+++ src/program.ts
@@ -1,7 +1,11 @@
-import {Command} from 'commander';
+import { Command, default as commander} from 'commander';
+import {Renderer} from 'xlsx-renderer';
+import { Stream } from 'stream';
+import * as fs from 'fs';
 const version = '0.0.1';
 
 let DEBUG = false;
+
 const writeDebug = (...params:any[])=> {
     if(DEBUG) {
         // noinspection TsLint
@@ -9,29 +13,57 @@ const writeDebug = (...params:any[])=> {
     }
 };
 
-const debugOption = (program: any) => {
+const mainCommand = (program:commander.Command, outputStream:Stream) =>{
+    const readModel = (file?:string)=>{
+        if(file) {
+            return fs.readFileSync(file, "utf8");
+        }
+        return null;
+    };
+
+    program.description('Generate spreadsheet based on <template> and [model]');
+    program.option('-o --output <filename>', 'write output into file');
+    program.option('-m --model <filename>', 'read file as a [model] (using both is denied)');
+    program.arguments('<template> [model]')
+        .action(async (template: string, modelRaw?: string) => {
+            writeDebug('Options:', program.opts());
+            writeDebug('Arguments:', {template, modelRaw});
+
+            if(program.model && modelRaw){
+                throw new Error('Using both `-m --model <filename>` and `[model]` is denied.');
+            }
+
+            const model = JSON.parse(modelRaw || readModel(program.model) ||'{}');
+
+            writeDebug('Model: ', model);
+
+
+            const renderer = new Renderer();
+            const result = await renderer.renderFromFile(template, model);
+
+            if(program.output){
+                // @ts-ignore
+                result.xlsx.writeFile(program.output);
+            }else{
+                // @ts-ignore
+                result.xlsx.write(outputStream);
+            }
+
+        });
+};
+
+const debugOption = (program: commander.Command) => {
     program.option('-d, --debug', 'output extra debugging');
     program.on('option:debug', () => {
         DEBUG = true;
-        writeDebug(program.opts());
     });
 };
 
-export const cli = (argv: string[])=> {
+export const cli = (argv: string[], outputStream: Stream)=> {
     const program = new Command();
     program.version(version);
-    program.description('Generate spreadsheet file to <output> based on <template> and [viewModel]');
-    program.usage('template output \'{"someData":[]}\'');
-    program.arguments('<template> <output> [viewModel]',)
-        .action((template:string, output:string, viewModelRaw?:string) => {
-            writeDebug('sxr args', template, output, viewModelRaw);
-            const viewModel = JSON.parse(viewModelRaw || '{}');
-            writeDebug('viewModel', viewModel);
 
-
-
-        });
-
+    mainCommand(program, outputStream);
     debugOption(program);
 
 
--- src/sxr.ts
+++ src/sxr.ts
@@ -1,3 +1,18 @@
 import {cli} from './program';
 
-cli(process.argv);
+
+if (process.stdin.isTTY) {
+    cli(process.argv, process.stdout);
+}
+else {
+    let stdin = '';
+    process.stdin.on('readable', () => {
+        const chunk = process.stdin.read();
+        if (chunk !== null) {
+            stdin += chunk;
+        }
+    });
+    process.stdin.on('end', () => {
+        cli([...process.argv, stdin], process.stdout);
+    });
+}
